cde270f99576965da5f83dd8e0de331de5aa32b2
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-01-30 12:07:03 -0700

    passing lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching (drivers, string) {
+  let matchingDrivers = [];
+  for (let item of drivers) {
+    if (string.toLowerCase() === item.toLowerCase()) {
+        matchingDrivers.push(item);
+  };
+};
+    return matchingDrivers;
+}; 
+
+function fuzzyMatch (drivers, string) {
+    let letterMatch = [];
+    for (let item of drivers) {
+        if (string[0] === item[0]) {
+            letterMatch.push(item);
+        };
+    };
+    return letterMatch;
+};
+
+function matchName (drivers, string) {
+    let driverByName = [];
+    for (let element of drivers) {
+        if (string.toLowerCase() === element.name.toLowerCase()) {
+            driverByName.push(element);   
+        };
+    };
+    return driverByName;
+};
